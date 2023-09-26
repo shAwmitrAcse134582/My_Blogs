8aeee60e63583b0d3e2ac15b1ce3aa7eb02ff3b5
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-09-27 03:04:04 +0600

    admin panel added

--- db_blog/config/config.php
+++ db_blog/config/config.php
@@ -1,7 +1,15 @@
 <?php
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "db_blog");
+// define("TITLE", "Training with live project");
+// define("KEYWORDS", "PHP Tutorial, JAVA Tutorial, Oracle Database");
+
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "db_blog");
+define("DB_USER", "id21316123_admin");
+define("DB_PASS", "Shawmitra123@");
+define("DB_NAME", "id21316123_my_db");
 define("TITLE", "Training with live project");
 define("KEYWORDS", "PHP Tutorial, JAVA Tutorial, Oracle Database");
+
